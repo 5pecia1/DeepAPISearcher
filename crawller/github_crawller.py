395da8e82037e156038e05a0d480b9bb3858edf3
5pecia1 <5o1.3an9@gmail.com>
2017-03-04 17:27:02 +0900

    add random sleep to more safety crawlling

--- crawller/github_crawller.py
+++ crawller/github_crawller.py
@@ -4,7 +4,7 @@ abca dfs.
 
 from bs4 import BeautifulSoup as bs
 
-import requests, time
+import requests, time, os, random
 
 def get_repo_list_a_tags(link):
     """
@@ -21,17 +21,22 @@ def get_repo_list_a_tags(link):
 
 if __name__ == "__main__":
 
-    url = 'https://github.com/search?l=&q=language%3AJava+stars%3A%3E1&ref=advsearch&type=Repositories&utf8=%E2%9C%93&p='
-    crawll_count = 100
-    page_count = int(crawll_count / 10)
-    sleep_time = 5
+    url = 'https://github.com/search?q=language%3AJava+stars%3A%3E%3D1&ref=searchresults&type=Repositories&utf8=%E2%9C%93&p='
+    crawll_count = 10
+    # crawll_count = 383177
+    page_count = int(crawll_count / 10) + 1 + (1 if (crawll_count % 10) > 0 else 0)
+    sleep_time_max_count = 10
 
-    for i in range(page_count):
-        i += 1
-        time.sleep(sleep_time)
+    for i in range(1, page_count):
         url_query = url + str(i)
         repo_list_a_tags = get_repo_list_a_tags(url_query)
 
         for repo_list_a_tag in repo_list_a_tags:
             repo_url = "/" + repo_list_a_tag.text
             print (repo_url)
+
+            print (os.system('cd project_data; git clone https://github.com' + repo_url + ".git"))
+            sleep_time = random.randrange(0, sleep_time_max_count)
+
+            time.sleep(sleep_time)
+            # print(os.wait())
