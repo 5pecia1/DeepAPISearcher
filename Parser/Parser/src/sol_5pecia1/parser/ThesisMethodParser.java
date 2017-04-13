043c66605e2ffc8b10467689ed3ecf9585437de3
5pecia1 <5o1.3an9@gmail.com>
2017-04-13 15:11:51 +0900

    parsing annotatoin like thesis

--- Parser/Parser/src/sol_5pecia1/parser/ThesisMethodParser.java
+++ Parser/Parser/src/sol_5pecia1/parser/ThesisMethodParser.java
@@ -1,7 +1,10 @@
 package sol_5pecia1.parser;
 
+import java.util.regex.Pattern;
+
 import com.github.javaparser.ast.Node;
 import com.github.javaparser.ast.body.MethodDeclaration;
+import com.github.javaparser.ast.comments.JavadocComment;
 import com.github.javaparser.ast.expr.MethodCallExpr;
 import com.github.javaparser.ast.expr.ObjectCreationExpr;
 import com.github.javaparser.symbolsolver.javaparsermodel.JavaParserFacade;
@@ -14,7 +17,7 @@ import com.github.javaparser.symbolsolver.resolution.typesolvers.ReflectionTypeS
 import sol_5pecia1.parser.methodinformation.ThesisSequenceVisitorImplements;
 
 /**
- * 하나의 메서드를 논문형식으로 Parsing한 String 및 List를 전달한다.
+ * 하나의 메서드를 논문형식으로 Parsing한 apiSequence 및 annotation을 갖는다.
  * 
  * @author sol
  *
@@ -22,63 +25,138 @@ import sol_5pecia1.parser.methodinformation.ThesisSequenceVisitorImplements;
 public class ThesisMethodParser {
 	private final static String OBJECT_CREATE = "new";
 	private String apiSequence = "";
+	private String annotation = "";
 	private ThesisSequenceVisitorImplements thesisSequenceVisitorImplements = new ThesisSequenceVisitorImplements();
 
 	public void visit(MethodDeclaration node, JavaParserTypeSolver sourceClassSolver) {
 		visit(node, sourceClassSolver, null);
 	}
 
-	//TODO 내부 메서드 값 처리하기.
-	public void visit(MethodDeclaration node, JavaParserTypeSolver sourceSolver,
-			JavaParserTypeSolver classSolver) {
+	public void visit(MethodDeclaration node, JavaParserTypeSolver sourceSolver, JavaParserTypeSolver classSolver) {
 		thesisSequenceVisitorImplements.visit(node, null);
+
 		CombinedTypeSolver combinedTypeSolver = new CombinedTypeSolver();
 		combinedTypeSolver.add(new ReflectionTypeSolver());
 		combinedTypeSolver.add(sourceSolver);
-		if (classSolver != null){
+
+		if (classSolver != null) {
 			combinedTypeSolver.add(classSolver);
 		}
-		
+
+		apiSequence = parsingApi(combinedTypeSolver);
+		annotation = parsingAnnotation();
+	}
+
+	private String parsingApi(CombinedTypeSolver combinedTypeSolver) {
 		StringBuilder builder = new StringBuilder();
-		for(Node visitedNode : thesisSequenceVisitorImplements.getNodeListSequenceAboutThesis()){
-			
+		for (Node visitedNode : thesisSequenceVisitorImplements.getNodeListSequenceAboutThesis()) {
 			if (visitedNode instanceof ObjectCreationExpr) {
-				ObjectCreationExpr creationExpr = (ObjectCreationExpr)visitedNode;
+				ObjectCreationExpr creationExpr = (ObjectCreationExpr) visitedNode;
 				Node classNode = creationExpr.getChildNodes().get(0);
-				builder
-				.append(classNode)
-				.append(".")
-				.append(OBJECT_CREATE)
-				.append("-");
+				builder.append(classNode).append(".").append(OBJECT_CREATE).append("-");
 			} else if (visitedNode instanceof MethodCallExpr) {
 				JavaParserFacade facade = JavaParserFacade.get(combinedTypeSolver);
-				MethodCallExpr callExpr = (MethodCallExpr)visitedNode;
+				MethodCallExpr callExpr = (MethodCallExpr) visitedNode;
 				System.out.println(callExpr);
 				Node methodNode = callExpr.getChildNodes().get(0);
 				String simpleApiName = "";
-				
-				if ("com.github.javaparser.ast.expr.SimpleName".equals(methodNode.getClass().getTypeName())) {//TODO 내부 메서드 값 추가!
-					SymbolReference<com.github.javaparser.symbolsolver.model.declarations.MethodDeclaration> reference = 
-							facade.solve(callExpr);
+
+				if ("com.github.javaparser.ast.expr.SimpleName".equals(methodNode.getClass().getTypeName())) {// TODO
+																												// 내부
+																												// 메서드
+																												// 값
+																												// 추가!
+					SymbolReference<com.github.javaparser.symbolsolver.model.declarations.MethodDeclaration> reference = facade
+							.solve(callExpr);
 					simpleApiName = reference.getCorrespondingDeclaration().getClassName();
 				} else {
 					Type typeOfTheNode = facade.getType(methodNode);
 					String apiName = typeOfTheNode.describe().toString();
 					simpleApiName = apiName.substring(apiName.lastIndexOf(".") + 1);
 				}
-				
-				builder
-				.append(simpleApiName)
-				.append(".")
-				.append(callExpr.getName())
-				.append("-");
+
+				builder.append(simpleApiName).append(".").append(callExpr.getName()).append("-");
 			}
 		}
-		
-		apiSequence = builder.substring(0, builder.length()-1);
+
+		return builder.toString();
 	}
 	
+	/**
+	 * filter about tag, todo, brackets
+	 * and return first line
+	 * 
+	 * @return
+	 */
+	private String parsingAnnotation() {
+		JavadocComment comment = thesisSequenceVisitorImplements.getComment();
+		String description = comment.parse().getDescription().toText();
+		String firstLine = "";
+		
+		description = parsingTodo(description);
+		firstLine = getFirstLine(description);
+		
+	System.out.println(firstLine);	
+		firstLine = parsingTag(firstLine);
+		firstLine = parsingBrackets(firstLine);
+		System.out.println(firstLine);
+		
+		return firstLine;
+	}
+	
+	private String getFirstLine(String string) {
+		String firstLine = "";
+		
+		if (!string.toLowerCase().contains("todo") && string.contains(".\n") || string.contains(". ")) {
+			String splited[] = string.split("([.]\n|[.] )");
+			firstLine= splited[0].replaceAll("\n", " ");
+		} else if (string.contains("\n")){
+			firstLine = string.substring(0, string.indexOf("\n"));
+		}
+		return firstLine;
+	}
+	
+	private String parsingTag(String string) {
+		if (Pattern.matches(".*<.+>.*", string)) {
+			return parsingTag(
+					string.substring(0, string.indexOf("<")) + 
+					string.substring(string.indexOf(">") + 1)
+					);
+		} else {
+			return string;
+		}
+	}
+	
+	/**
+	 * TODO 좀 더 확실히 튜닝 가능한지 확인하기, class 사용해서 괄호안의 객체 정보 얻어오기
+	 * @param string
+	 * @return
+	 */
+	private String parsingTodo(String string) {
+		String firstLine = getFirstLine(string);
+		if (firstLine.toLowerCase().contains("todo")) {
+			return parsingTodo(string.substring(firstLine.length() + 1));
+		} else {
+			return string;
+		}
+	}
+	
+	private String parsingBrackets(String string) {
+		if (Pattern.matches(".*[{].+[}].*", string)) {
+			return parsingTag(
+					string.substring(0, string.indexOf("{")) + 
+					string.substring(string.indexOf("}") + 1)
+					);
+		} else {
+			return string;
+		}
+	}
+
 	public String getAPISequence() {
 		return apiSequence;
+	}
+
+	public String getAnnotaion() {
+		return annotation;
 	}
 }
