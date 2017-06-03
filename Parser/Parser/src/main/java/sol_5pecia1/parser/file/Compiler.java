d10560ff7638696ee4cf07c6e0bf62e2ea80899e
5pecia1 <5o1.3an9@gmail.com>
2017-06-03 22:15:04 +0900

    make compiler

--- Parser/Parser/src/main/java/sol_5pecia1/parser/file/Compiler.java
+++ Parser/Parser/src/main/java/sol_5pecia1/parser/file/Compiler.java
@@ -1,6 +1,13 @@
 package sol_5pecia1.parser.file;
 
+import com.github.javaparser.JavaParser;
+import com.github.javaparser.ast.CompilationUnit;
+
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.NoSuchElementException;
 import java.util.function.Function;
 
 /**
@@ -10,7 +17,49 @@ public class Compiler implements Function<File, Boolean> {
 
     @Override
     public Boolean apply(File file) {
+        System.out.println("Start compile about : " + file);
 
-        return null;
+        try {
+            FileInputStream in = new FileInputStream(file);
+            CompilationUnit cu = JavaParser.parse(in);
+
+            String packageName =
+                    cu.getPackageDeclaration().get().getNameAsString();
+
+            String osName = System.getProperty("os.name");
+            Runtime rt = Runtime.getRuntime();
+            File filePath = file.getParentFile();
+
+            for (int i = packageName.split("[.]").length; i > 0 ; i--) {
+                filePath = filePath.getParentFile();
+            }
+            String pn = packageName.replaceAll("[.]", "/");
+            String[] cmd = (osName.toLowerCase().startsWith("window"))
+                    ? new String[]{
+                    "cmd.exe", "/c", "cd ", filePath.toString(),
+                    "&&",  "javac", pn + "/" + file.getName()}
+                    : new String[]{
+                    "/bin/sh", "/c", "cd ", filePath.toString(),
+                    "&&",  "javac", pn + "/" + file.getName()};
+
+            Process process = rt.exec(cmd);
+
+            process.getErrorStream().close();
+            process.getInputStream().close();
+            process.getOutputStream().close();
+
+            int exitResult = process.waitFor();
+
+            return exitResult != -1;
+        } catch (NoSuchElementException nsee) {
+            nsee.printStackTrace();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        return false;
     }
 }
